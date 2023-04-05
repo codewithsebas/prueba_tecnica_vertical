9ec8ce7ef066d6d7fa77260b9b1917a1ed855708
Sebastián Giraldo <joabgiraldo@gmail.com>
2023-04-05 17:24:33 -0500

    Prueba tecnica - Vertical

---
+++ index.js
@@ -0,0 +1,27 @@
+function test1(num) {
+    const divisible = (divisor, num) => num % divisor === 0
+
+    if(divisible(3, num) && divisible(5, num)) {
+        return 'Es multiplo de 3 y multiplo de 5'
+    }
+
+    if(divisible(3, num)) {
+        return 'Es multiplo de 3'
+    }
+
+    if(divisible(5, num)){
+        return 'Es multiplo de 5';
+    }
+
+    return 'No multiplo'    
+}
+
+function num(numRecord) {
+    for(let i = 1; i < numRecord; i++) {
+        console.log(`${i}: ${test1(i)}`);
+    } 
+} 
+
+num(101)
+
+module.exports = num;
